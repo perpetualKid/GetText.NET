0beede414a401eb14ed7b74fe0019da2955ab0e2
perpetualKid <perpetualKid@azuros.net>
2020-08-23 16:55:51 +0200

    Adding Extractor project

---
+++ src/GetText.Extractor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetText.Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World!", args);
+        }
+    }
+}
